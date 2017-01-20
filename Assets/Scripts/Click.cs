51403eb968769e8123da3660bf258b7c30c73c1c
Benjamin KayEight <Benjamin KayEight>
2017-01-20 14:18:47 +0000

    first commit

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+    public UnityEngine.UI.Text goldDisplay;
+    public int gold = 0;
+    public int goldperclick = 1;
+
+    private void Update(){
+        goldDisplay.text = "Gold: " + gold;
+        
+    }
+
+    public void Clicked()
+    {
+        gold += goldperclick;
+        
+    }
+
+}
